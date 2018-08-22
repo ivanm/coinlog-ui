dd8e8ca2f87af68e2af3c76eb07405028972da47
Ivan Mayoral <iimayoral@gmail.com>
2018-08-21 22:03:28 -0500

    Adding Pure CSS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Index = () => {
-  return <div>Hello React!</div>;
-};
+const Index = () =>
+  <div>
+    <a className="pure-button" href="#">Hello World</a>
+  </div>;
 
 ReactDOM.render(<Index />, document.getElementById("app"));
