f88e3717b5e910f1600bb1f0be0d210fb11d51ed
Ivan Mayoral <iimayoral@gmail.com>
2018-08-24 22:16:15 -0500

    Adding basic grid and title

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const Index = () =>
-  <div>
-    <a className="pure-button" onClick={ () => alert('hi') }>Hello Worlds</a>
-  </div>;
+<div className="container">
+    <div className="left-pane">
+        <div className="nav-title">
+            coinlog<span className="title-dot">.</span>sh
+        </div>
+    </div>
+    <div class="main-pane">
+    </div>
+</div>;
 
 ReactDOM.render(<Index />, document.getElementById("app"));
