cd1e698f58a5d49f7630b48710240e609fb7ef45
Ivan Mayoral <iimayoral@gmail.com>
2020-05-26 13:16:24 -0500

    Adding hot loading

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import { Provider } from 'react-redux';
-import configureStore from './redux/configureStore.js';
-
-let store = configureStore();
+import ProvidedApp from './components/ProvidedApp';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <ProvidedApp/>,
     document.getElementById('app')
 );
